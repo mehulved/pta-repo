9819407f183d0f3ae89c8d12f271da0b44ec1931
Mehul Ved <mehul.ved@nexsales.com>
2017-03-03 19:40:35 +0530

    fully working hello world app on python.
    This commit adds more steps to get the hello world app working.
    The app is written in falcon framework and requires gunicorn to work
    and thus necessitates some additional steps to get it work with
    virtualenv.
    We have also added script to install nginx.

---
+++ packer/scripts/nginx.sh
@@ -0,0 +1,4 @@
+#!/bin/bash
+
+sudo DEBIAN_FRONTEND="noninteractive" apt-get install -y nginx-light
+
--- packer/scripts/python.sh
+++ packer/scripts/python.sh
@@ -1,5 +1,8 @@
 #!/bin/bash
 
+# Version of the app
+PYTHON_APP_VERSION=0.2
+
 # User that the app runs as
 PYTHON_APP_USER=pyuser
 
@@ -16,7 +19,7 @@ then
         echo "$PYTHON_APP_USER created."
     fi
 fi
-if [[ ! -d $PYTHON_APP_HOME ]]
+if [[ ! -d $PYTHON_APP_DIR ]]
 then
     sudo mkdir $PYTHON_APP_DIR
     if [[ $? -eq 0 ]]
@@ -29,3 +32,19 @@ fi
 # Install essential packages
 sudo DEBIAN_FRONTEND="noninteractive" apt-get install -y python3 python3-virtualenv python-virtualenv
 sudo -u $PYTHON_APP_USER /usr/bin/virtualenv -p python3 $PYTHON_APP_DIR/venv
+sudo wget https://github.com/mehulved/pta-hello-app/archive/v$PYTHON_APP_VERSION.tar.gz -O /usr/local/src/hello-app-v$PYTHON_APP_VERSION.tar.gz
+sudo -u $PYTHON_APP_USER tar xzvf /usr/local/src/hello-app-v$PYTHON_APP_VERSION.tar.gz -C /tmp
+sudo mv /tmp/pta-hello-app-$PYTHON_APP_VERSION/* $PYTHON_APP_DIR/
+sudo mv /tmp/helloapp.service /etc/systemd/system/helloapp.service
+sudo mv /tmp/helloapp.socket /etc/systemd/system/helloapp.socket
+sudo mv /tmp/helloapp.tmpfiles.conf /usr/lib/tmpfiles.d/helloapp.conf
+sudo sed -i "s*<PYTHON_APP_DIR>*$PYTHON_APP_DIR*g" /etc/systemd/system/helloapp.*
+sudo sed -i "s/<PYTHON_APP_USER>/$PYTHON_APP_USER/g" /etc/systemd/system/helloapp.*
+sudo sed -i "s/<PYTHON_APP_USER>/$PYTHON_APP_USER/g" /usr/lib/tmpfiles.d/helloapp.conf
+set -x
+sudo -u $PYTHON_APP_USER PATH=$PYTHON_APP_DIR/venv/bin/:$PATH VIRTUAL_ENV=$PYTHON_APP_DIR/venv/ $PYTHON_APP_DIR/venv/bin/pip install -vvv -r $PYTHON_APP_DIR/requirements.txt
+set +x
+sudo systemctl enable helloapp.socket
+sudo systemctl enable helloapp.service
+sudo systemctl start helloapp.socket
+sudo systemctl start helloapp.service
