d2de82e70ef6503ee213bf7cc7e5c14707e7e658
Mehul Ved <mehul.ved@nexsales.com>
2017-03-03 22:01:20 +0530

    Nginx configuration to proxy the python app.
    We setup nginx to proxy the requests to python app running with
    gunicorn on non-http port.

--- packer/scripts/nginx.sh
+++ packer/scripts/nginx.sh
@@ -1,4 +1,9 @@
 #!/bin/bash
 
 sudo DEBIAN_FRONTEND="noninteractive" apt-get install -y nginx-light
+sudo mv /tmp/helloapp.nginx.conf /etc/nginx/sites-available/helloapp
+sudo ln -s /etc/nginx/sites-available/helloapp /etc/nginx/sites-enabled/helloapp
+sudo rm /etc/nginx/sites-enabled/default
+sudo nginx -t
+sudo systemctl restart nginx
 
